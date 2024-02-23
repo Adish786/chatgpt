5ab9793f68fe274ea1b6ee62ba8dac4c331d1284
Adish Ansari <adishansari786@gmail.com>
2024-02-23 19:01:05 +0530

    chatpt

--- src/main/java/com/chatgptbot/ChatgptBotApplication.java
+++ src/main/java/com/chatgptbot/ChatgptBotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatgptBotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChatgptBotApplication.class, args);
 	}
